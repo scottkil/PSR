84a528ceca3fb1c13fa1c7e3eb0610d64d1a8c03
scottkil <scottkilianski@gmail.com>
2025-09-09 18:44:37 -0400

    psr_prepAndRunQM now outputs the number of good clusters in a recording

--- Functions/psr_prepAndRunQM.m
+++ Functions/psr_prepAndRunQM.m
@@ -1,18 +1,19 @@
-function psr_prepAndRunQM(ksdir)
+function gc = psr_prepAndRunQM(ksdir)
 %% psr_prepAndRunQM Prepares recording directory for quality metric functions and then runs them
 %
 % INPUTS:
 %   ksdir - kilosort output directory
 %
 % OUTPUTS:
-%   None
+%   gc - 'good' clusters
 %
 % Written by Scott Kilianski
-% Updated on 2025-09-05
+% Updated on 2025-09-09
 % ------------------------------------------------------------ %
 %% ---- Function Body Here ---- %%%
 psr_calcRMS(fullfile(ksdir,'combined.bin'),256);
 psr_getSpikeAmps(ksdir);
 psr_checkClusters(ksdir);
-psr_applyQM(ksdir);
+goodLog = psr_applyQM(ksdir);
+gc = sum(goodLog);
 end % function end
